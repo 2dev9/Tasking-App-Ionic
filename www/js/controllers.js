1761b1fac3308cb7ba66420c6a951b9b9e0fef50
joselevelsup <hipnotickid12@gmail.com>
2017-06-22 18:19:32 -0400

    Adding the last files

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,36 @@
-angular.module('starter.controllers', [])
+angular.module('starter.controllers', ["ionic.native"])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('TasksCtrl', function($scope, Tasks) {
+.controller('TasksCtrl', function($scope, Tasks, $ionicModal, $cordovaToast) {
   $scope.tasks = Tasks.all();
   $scope.remove = function(task) {
     Tasks.remove(task);
   };
+
+  $ionicModal.fromTemplateUrl("templates/modal.html", {
+    scope: $scope,
+    animation: "slide-in-up"
+  }).then(function(modal){
+    $scope.modal = modal;
+  });
+
+
+
+
+
+
+  $scope.new = {
+    taskName: "",
+    taskDesc: ""
+  };
+    $scope.newTask = function(){
+        if(Tasks.create($scope.new)){
+          $cordovaToast.show("Task Created", "short", "bottom");
+          $scope.modal.hide();
+        }
+    }
+
 })
 
 .controller('TaskDetailCtrl', function($scope, $stateParams, Tasks) {
